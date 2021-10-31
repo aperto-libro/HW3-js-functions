406f977ae6a978d51558b4f02f732059a606d402
Vitalii Slobodoniuk <slvitalik24@ukr.net>
2021-10-31 14:05:09 +0200

    added tast 4

---
+++ script.js
@@ -0,0 +1,32 @@
+console.log('------------- # 4');
+
+function getSum(a, b) {
+  return a + b;
+}
+
+function getDiff(a, b) {
+  return a - b;
+}
+
+function getMulti(a, b) {
+  return a * b;
+}
+
+function getDiv(a, b) {
+  return a / b;
+}
+
+console.log(getSum(10, 5));
+console.log(getDiff(15, 11));
+console.log(getMulti(6, 8));
+console.log(getDiv(105, 15));
+
+console.log('------------- # 5');
+
+console.log('------------- # 6');
+
+console.log('------------- # 7');
+
+console.log('------------- # 8');
+
+console.log('------------- # 9');
